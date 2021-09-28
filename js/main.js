2d406424f461ecd8da200467c308cbc06bfab485
Hisime <rihisime@gmail.com>
2021-09-28 13:56:17 +0300

    Нужно больше функций (исправления)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+const isInvalidFloat = (number) => number < 0 || !(typeof number === 'number') || Number.isNaN(number);
+const isInvalidInteger = (number) => number < 0 || !Number.isInteger(number);
+
 const getRandomInteger = (min, max) => {
+  if (isInvalidInteger(min) || isInvalidInteger(max)) {
+    return 'Передан неверный аргумент. Аргумент должен быть больше или равен нулю и являться целым числом.';
+  }
   const randomNumber = min + Math.random() * (max + 1 - min);
   return Math.floor(randomNumber);
 };
 
 const getRandomFloat = (min, max, length) => {
+  if (isInvalidFloat(min) || isInvalidFloat(max) || isInvalidInteger(length)) {
+    return 'Передан неверный аргумент. Аргумент должен быть больше или равен нулю и являться числом. Третий аргумент должен быть только целым числом.';
+  }
   const randomNumber = min + Math.random() * (max - min);
-  return randomNumber.toFixed(length);
+  return Number(randomNumber.toFixed(length));
 };
 
 getRandomInteger(1, 10);

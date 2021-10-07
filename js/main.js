bd94ccc17ae71c3764b4e671c3dcfc277bcc658b
Hisime <rihisime@gmail.com>
2021-10-08 00:52:13 +0300

    Больше деталей (исправления)

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ getRandomFloat(1.1, 1.2, 10);
 
 const ADVERT_COUNT = 10;
 
-const titles = [
+const TITLES = [
   'Домик у моря',
   'Квартира с видом на залив',
   'Уютная студия  центре города',
@@ -30,17 +30,17 @@ const titles = [
   'Комната в квартире с отличными соседями',
 ];
 
-const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
-const checkTimes = ['12:00', '13:00', '14:00'];
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
 
-const descriptions = [
+const DESCRIPTIONS = [
   'Квартира в доме-памятнике купца Полежаева. В этом доме режиссер В. Бортко снимал в сцены для фильма по роману М.А. Булгакова «Мастер и Маргарита».',
   'Квартира с прекрасным видом на Канал Грибоедова в доме начала XX века. В историческом центре, в непосредственной близости от главных достопримечательностей.',
   'Старинный петербургский дом 1900 г. постройки. Совсем недавно в нём были реконструированы мини-студии, интересные исторические детали интерьера были бережно восстановлены.',
 ];
 
-const features = [
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -50,7 +50,7 @@ const features = [
 ];
 
 
-const photos = [
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
@@ -65,37 +65,40 @@ const shuffle = (array) => {
   array.sort(() => Math.random() - 0.5);
 };
 
-const getAvatarUrl = (number) => {
+const getAvatarUrl = (item, number) => {
   number++;
-  if (number.length >= 10) {
+  if (number >= 10) {
     return `img/avatars/user${number}.png`;
   }
   return `img/avatars/user0${number}.png`;
 };
+
+const AVATARS = Array.from({ length: ADVERT_COUNT}, getAvatarUrl);
+shuffle(AVATARS);
 
 const createAdvert = (item, index) => {
   const newLocation = {
     lat: getRandomFloat(35.65, 35.7, 5),
     lng: getRandomFloat(139.7, 139.8, 5),
   };
-  shuffle(features);
-  shuffle(photos);
+  shuffle(FEATURES);
+  shuffle(PHOTOS);
   return {
     author: {
-      avatar: getAvatarUrl(index),
+      avatar: AVATARS[index],
     },
     offer: {
-      title: getRandomArrayElement(titles),
+      title: getRandomArrayElement(TITLES),
       address: `${newLocation.lat}, ${newLocation.lng}`,
       price: getRandomInteger(0, 1000000),
-      type: getRandomArrayElement(types),
+      type: getRandomArrayElement(TYPES),
       rooms: getRandomInteger(1, 20),
       guests: getRandomInteger(0, 10),
-      checkin: getRandomArrayElement(checkTimes),
-      checkout: getRandomArrayElement(checkTimes),
-      features: features.slice(0, getRandomInteger(0, features.length)),
-      description: getRandomArrayElement(descriptions),
-      photos: photos.slice(0, getRandomInteger(0, photos.length)),
+      checkin: getRandomArrayElement(CHECK_TIMES),
+      checkout: getRandomArrayElement(CHECK_TIMES),
+      features: FEATURES.slice(0, getRandomInteger(0, FEATURES.length)),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: PHOTOS.slice(0, getRandomInteger(0, PHOTOS.length)),
     },
     location: {
       lat: newLocation.lat,
